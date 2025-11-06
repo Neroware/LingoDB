f6498a4dee5b3fd660ef2d584a514fc249ac7719
Raoul Zebisch <raoul.zebischq@uni-a.de>
2025-11-06 15:05:48 +0000

    Updated StateMembersAttr parser for graph dialect

--- src/compiler/Dialect/Graph/GraphTypes.cpp
+++ src/compiler/Dialect/Graph/GraphTypes.cpp
@@ -1,6 +1,7 @@
 #include "lingodb/compiler/Dialect/Graph/GraphDialect.h"
 #include "lingodb/compiler/Dialect/Graph/GraphOps.h"
 #include "lingodb/compiler/Dialect/Graph/GraphOpsTypes.h"
+#include "lingodb/compiler/Dialect/SubOperator/SubOperatorDialect.h"
 
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
@@ -12,74 +13,84 @@ using namespace lingodb::compiler::dialect::graph;
 namespace {
    using namespace lingodb::compiler::dialect;
    static mlir::LogicalResult parseStateMembers(mlir::AsmParser& parser, subop::StateMembersAttr& stateMembersAttr) {
+      auto& memberManager = parser.getContext()->getOrLoadDialect<subop::SubOperatorDialect>()->getMemberManager();
       if (parser.parseLSquare()) return mlir::failure();
-      std::vector<mlir::Attribute> names;
-      std::vector<mlir::Attribute> types;
+      llvm::SmallVector<subop::Member> members;
       while (true) {
          if (!parser.parseOptionalRSquare()) { break; }
          llvm::StringRef colName;
          mlir::Type t;
          if (parser.parseKeyword(&colName) || parser.parseColon() || parser.parseType(t)) { return mlir::failure(); }
-         names.push_back(parser.getBuilder().getStringAttr(colName));
-         types.push_back(mlir::TypeAttr::get(t));
+         members.push_back(memberManager.createMemberDirect(colName.str(), t));
          if (!parser.parseOptionalComma()) { continue; }
          if (parser.parseRSquare()) { return mlir::failure(); }
          break;
       }
-      stateMembersAttr = subop::StateMembersAttr::get(parser.getContext(), parser.getBuilder().getArrayAttr(names), parser.getBuilder().getArrayAttr(types));
+      stateMembersAttr = subop::StateMembersAttr::get(parser.getContext(), members);
       return mlir::success();
    }
    static void printStateMembers(mlir::AsmPrinter& p, subop::StateMembersAttr stateMembersAttr) {
+      auto& memberManager = stateMembersAttr.getContext()->getOrLoadDialect<subop::SubOperatorDialect>()->getMemberManager();
       p << "[";
       auto first = true;
-      for (size_t i = 0; i < stateMembersAttr.getNames().size(); i++) {
-         auto name = mlir::cast<mlir::StringAttr>(stateMembersAttr.getNames()[i]).str();
-         auto type = mlir::cast<mlir::TypeAttr>(stateMembersAttr.getTypes()[i]).getValue();
+      for (subop::Member m : stateMembersAttr.getMembers()) {
          if (first) {
             first = false;
          } else {
             p << ", ";
          }
-         p << name << " : " << type;
+         p << memberManager.getName(m) << " : " << memberManager.getType(m);
       }
       p << "]";
    }
 } // namespace
 
 subop::StateMembersAttr graph::GraphType::getMembers() {
-   std::vector<mlir::Attribute> names;
-   std::vector<mlir::Attribute> types;
-   names.insert(names.end(), getNodeMembers().getNames().begin(), getNodeMembers().getNames().end());
-   names.insert(names.end(), getEdgeMembers().getNames().begin(), getEdgeMembers().getNames().end());
-   types.insert(types.end(), getNodeMembers().getTypes().begin(), getNodeMembers().getTypes().end());
-   types.insert(types.end(), getEdgeMembers().getTypes().begin(), getEdgeMembers().getTypes().end());
-   return subop::StateMembersAttr::get(this->getContext(), mlir::ArrayAttr::get(this->getContext(), names), mlir::ArrayAttr::get(this->getContext(), types));
+   llvm::SmallVector<subop::Member> combined;
+   combined.reserve(getNodeMembers().getMembers().size() + getEdgeMembers().getMembers().size());
+   for (const auto& member : getNodeMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getEdgeMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   return subop::StateMembersAttr::get(this->getContext(), combined);
 }
 subop::StateMembersAttr graph::NodeRefType::getMembers() {
-   std::vector<mlir::Attribute> names;
-   std::vector<mlir::Attribute> types;
-   names.insert(names.end(), getNodeMembers().getNames().begin(), getNodeMembers().getNames().end());
-   names.insert(names.end(), getIncomingMembers().getNames().begin(), getIncomingMembers().getNames().end());
-   names.insert(names.end(), getOutgoingMembers().getNames().begin(), getOutgoingMembers().getNames().end());
-   names.insert(names.end(), getPropertyMembers().getNames().begin(), getPropertyMembers().getNames().end());
-   types.insert(types.end(), getNodeMembers().getTypes().begin(), getNodeMembers().getTypes().end());
-   types.insert(types.end(), getIncomingMembers().getTypes().begin(), getIncomingMembers().getTypes().end());
-   types.insert(types.end(), getOutgoingMembers().getTypes().begin(), getOutgoingMembers().getTypes().end());
-   types.insert(types.end(), getPropertyMembers().getTypes().begin(), getPropertyMembers().getTypes().end());
-   return subop::StateMembersAttr::get(this->getContext(), mlir::ArrayAttr::get(this->getContext(), names), mlir::ArrayAttr::get(this->getContext(), types));
+   llvm::SmallVector<subop::Member> combined;
+   combined.reserve(getNodeMembers().getMembers().size() 
+      + getIncomingMembers().getMembers().size()
+      + getOutgoingMembers().getMembers().size());
+   for (const auto& member : getNodeMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getIncomingMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getOutgoingMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   return subop::StateMembersAttr::get(this->getContext(), combined);
 }
 subop::StateMembersAttr graph::EdgeRefType::getMembers() {
-   std::vector<mlir::Attribute> names;
-   std::vector<mlir::Attribute> types;
-   names.insert(names.end(), getEdgeMembers().getNames().begin(), getEdgeMembers().getNames().end());
-   names.insert(names.end(), getFromMembers().getNames().begin(), getFromMembers().getNames().end());
-   names.insert(names.end(), getToMembers().getNames().begin(), getToMembers().getNames().end());
-   names.insert(names.end(), getPropertyMembers().getNames().begin(), getPropertyMembers().getNames().end());
-   types.insert(types.end(), getEdgeMembers().getTypes().begin(), getEdgeMembers().getTypes().end());
-   types.insert(types.end(), getFromMembers().getTypes().begin(), getFromMembers().getTypes().end());
-   types.insert(types.end(), getToMembers().getTypes().begin(), getToMembers().getTypes().end());
-   types.insert(types.end(), getPropertyMembers().getTypes().begin(), getPropertyMembers().getTypes().end());
-   return subop::StateMembersAttr::get(this->getContext(), mlir::ArrayAttr::get(this->getContext(), names), mlir::ArrayAttr::get(this->getContext(), types));
+   llvm::SmallVector<subop::Member> combined;
+   combined.reserve(getEdgeMembers().getMembers().size() 
+      + getFromMembers().getMembers().size()
+      + getToMembers().getMembers().size()
+      + getPropertyMembers().getMembers().size());
+   for (const auto& member : getEdgeMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getFromMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getFromMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   for (const auto& member : getPropertyMembers().getMembers()) {
+      combined.push_back(member);
+   }
+   return subop::StateMembersAttr::get(this->getContext(), combined);
 }
 
 #define GET_TYPEDEF_CLASSES
