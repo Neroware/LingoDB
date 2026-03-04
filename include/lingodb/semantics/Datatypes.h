1e8b5649b08c3a8f5379aa9e96deba95b2693284
Raoul Zebisch <raoul.zebischq@uni-a.de>
2026-03-04 15:43:45 +0000

    Node Entry helpers

--- include/lingodb/semantics/Datatypes.h
+++ include/lingodb/semantics/Datatypes.h
@@ -6,20 +6,20 @@ namespace lingodb::semantics {
 // Supported XSD Datatypes following alphabetical ordering as on the table
 // at https://www.w3.org/2011/rdf-wg/wiki/XSD_Datatypes
 enum XSDType {
-    XSD_UNDEFINED = 0,
-    XSD_TYPE_BOOLEAN = 3,
-    XSD_TYPE_BYTE = 4,
-    XSD_TYPE_DOUBLE = 10,
+    UNKNOWN = -1,
+    XSD_ANY_URI = 0,
+    XSD_TYPE_BOOLEAN = 2,
+    XSD_TYPE_BYTE = 3,
+    XSD_TYPE_DOUBLE = 7,
     XSD_TYPE_FLOAT = 11,
-    XSD_TYPE_INT = 18,
-    XSD_TYPE_INTEGER = 19,
-    XSD_TYPE_LONG = 21,
-    XSD_TYPE_SHORT = 30,
-    XSD_TYPE_STRING = 31,
-    XSD_TYPE_UNSIGNED_BYTE = 34,
-    XSD_TYPE_UNSIGNED_INT = 35,
-    XSD_TYPE_UNSIGNED_LONG = 36,
-    XSD_TYPE_UNSIGNED_SHORT = 37,
+    XSD_TYPE_INT = 21,
+    XSD_TYPE_LONG = 24,
+    XSD_TYPE_SHORT = 36,
+    XSD_TYPE_STRING = 37,
+    XSD_TYPE_UNSIGNED_BYTE = 40,
+    XSD_TYPE_UNSIGNED_INT = 41,
+    XSD_TYPE_UNSIGNED_LONG = 42,
+    XSD_TYPE_UNSIGNED_SHORT = 43,
 };
 
 } // namespace lingodb::semantics
