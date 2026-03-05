320169799b2aacf6e33bb2a4bc6f227522e0b98a
Raoul Zebisch <raoul.zebisch@uni-a.de>
2026-03-05 15:45:01 +0000

    RdfGraph struct

--- include/lingodb/semantics/RdfGraph.h
+++ include/lingodb/semantics/RdfGraph.h
@@ -1,17 +1,26 @@
 #ifndef LINGODB_SEMANTICS_RDFGRAPH_H
 #define LINGODB_SEMANTICS_RDFGRAPH_H
 
-#include "rdf4cpp.hpp"
+#include <rdf4cpp.hpp>
+#include "lingodb/runtime/Graph/PropertyGraph.h"
 
 using namespace rdf4cpp;
 
 namespace lingodb::semantics {
 
+struct extra_namespaces {
+    const Namespace PI = Namespace("https://www.uni-augsburg.de/de/fakultaet/fai/informatik/prof/pi#");
+    const Namespace SOBOT = Namespace("https://www.forsocialrobots.de/ontologies/sobots.owl#");
+    const Namespace LINGODB = Namespace("https://www.lingo-db.com/rdf#");
+};
 struct RdfGraph {
-    IRI foo() {
-        auto iri = IRI{"http://ex.com/MyGraph"};
-        return iri;
-    }
+    IRI name;
+    runtime::PropertyGraph* storage;
+    std::unordered_map<IRI, uint32_t> iris;
+    std::unordered_map<BlankNode, uint32_t> bnodes;
+    std::unordered_map<IRI, uint32_t> relTypes;
+    std::unordered_map<IRI, uint32_t> literalTypes;
+    static RdfGraph create(const Graph& rdfGraph, const IRI& name);
 };
 
 }
