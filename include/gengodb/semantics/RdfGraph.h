879b19fa0eb34c8ef0f766126e64f0b07b3e359b
Raoul Zebisch <raoul.zebisch@uni-a.de>
2026-03-13 22:59:35 +0000

    RDF Graph storage in Property graph

--- include/gengodb/semantics/RdfGraph.h
+++ include/gengodb/semantics/RdfGraph.h
@@ -16,37 +16,43 @@ struct extra_namespaces {
     const Namespace LINGODB = Namespace("https://www.lingo-db.com/rdf#");
     const Namespace GENGODB = Namespace("https://github.com/Neroware/LingoDB#");
 };
-class ResourceDictionary {
+class IriDictionary {
 private:
-    std::unordered_map<Node, int32_t> res_to_id;
-    std::vector<Node> id_to_res;
+    std::unordered_map<IRI, int32_t> iri_to_id;
+    std::vector<IRI> id_to_iri;
 public:
-    ResourceDictionary(const std::initializer_list<Node>& l) {
+    IriDictionary(const std::initializer_list<IRI>& l) {
         for (auto it = l.begin(); it != l.end(); it++) {
-            int32_t id = static_cast<int32_t>(id_to_res.size());
-            id_to_res.push_back(*it);
-            res_to_id.emplace(id_to_res.back(), id);
+            int32_t id = static_cast<int32_t>(id_to_iri.size());
+            id_to_iri.push_back(*it);
+            iri_to_id.emplace(id_to_iri.back(), id);
         }
     }
-    ResourceDictionary() {}
-    ~ResourceDictionary() {}
-    int32_t get_or_insert(const Node& res) {
-        auto it = res_to_id.find(res);
-        if (it != res_to_id.end())
-            return it->second;
-        int32_t id = static_cast<int32_t>(id_to_res.size());
-        id_to_res.push_back(res);
-        res_to_id.emplace(id_to_res.back(), id);
+    IriDictionary() {}
+    ~IriDictionary() {}
+    int32_t insert(const IRI& iri) {
+        int32_t id = static_cast<int32_t>(id_to_iri.size());
+        id_to_iri.push_back(iri);
+        iri_to_id.emplace(id_to_iri.back(), id);
         return id;
     }
-    int32_t get_safe(const Node& res) const {
-        auto it = res_to_id.find(res);
-        if (it == res_to_id.end())
+    int32_t get_or_insert(const IRI& iri) {
+        auto it = iri_to_id.find(iri);
+        if (it != iri_to_id.end())
+            return it->second;
+        int32_t id = static_cast<int32_t>(id_to_iri.size());
+        id_to_iri.push_back(iri);
+        iri_to_id.emplace(id_to_iri.back(), id);
+        return id;
+    }
+    int32_t get_safe(const IRI& iri) const {
+        auto it = iri_to_id.find(iri);
+        if (it == iri_to_id.end())
             return -1;
         return it->second;
     }
-    size_t size() const { return id_to_res.size(); }
-};
+    size_t size() const { return id_to_iri.size(); }
+}; // IriDictionary
 struct RdfGraph;
 class RdfGraphRegistry {
 private:
@@ -100,51 +106,68 @@ struct RdfDatatypeInlineHelper {
         *ptr = v;
     }
 };
+class NodeHelper {
+private:
+    RdfGraph* g;
+public:
+    NodeHelper(RdfGraph* g) : g(g) {}
+    inline void ensureNode();
+    inline int32_t resolveNode(const BlankNode& b);
+    inline int32_t resolveNode(const IRI& iri);
+    inline int32_t resolvePredicate(const IRI& p);
+    inline void addLiteral(int32_t sid, int32_t pid, const Literal& o);
+};
 struct RdfGraph {
     IRI name;
     runtime::PropertyGraph* storage;
-    ResourceDictionary nodes;
-    ResourceDictionary relations;
-    ResourceDictionary literalTypes;
+    IriDictionary nodes;
+    IriDictionary relations;
+    IriDictionary literalTypes;
+    std::unordered_map<std::string_view, int32_t> bnodes;
+    RdfGraph(const IRI& name, runtime::PropertyGraph* storage) 
+        : name(name), storage(storage), nodeHelper(this) {}
     static RdfGraph create(const IRI& name) {
         auto* storage = runtime::PropertyGraph::create(DEFAULT_CAPACITY, DEFAULT_CAPACITY, DEFAULT_CAPACITY);
-        RdfGraph graph{.name = name, .storage = storage, .nodes = {}, .relations = {}, .literalTypes = {}};
+        RdfGraph graph(name, storage);
         RdfGraphRegistry::singleton()->add(graph);
         return graph;
     }
     static RdfGraph create(const IRI& name, const Graph& rdfGraph);
     static RdfGraph create(const IRI& name, void* node_ptr, size_t node_l, void* rel_ptr, size_t rel_l, void* prop_ptr, size_t prop_l);
     void addTriple(const Node& s, const Node& p, const Node& o) {
-        int32_t sid = nodes.get_or_insert(s);
-        if (storage->nodeCounter < nodes.size()) {
-            storage->addNode();
-        }
-        int32_t oid = nodes.get_or_insert(o);
-        if (storage->nodeCounter < nodes.size()) {
-            storage->addNode();
-        }
-        int32_t pid = relations.get_or_insert(p);
-        storage->addRelationship(sid, oid, pid);
-        if (o.is_literal()) {
-            RdfDatatypeInlineHelper inlineHelper; 
-            auto l = o.as_literal();
-            auto datatype = literalTypes.get_or_insert(l.datatype());
-            uint64_t v = 0;
-            if (!inlineHelper.isInlined(l.datatype())) {
-                // TODO
-                assert(false && "only inlined literals supported");
-            }
-            else {
-                inlineHelper.inlineValue(&v, l.value(), l.datatype());
-            }
-            storage->addNodeProperty(oid, DATATYPE_PROPERTY_KEY, datatype, v);
+        if (!p.is_iri()) assert(false && "predicate must be an IRI");
+        const auto& pred = p.as_iri();
+        if (s.is_iri()) {
+            const auto& subj = s.as_iri();
+            if (o.is_iri())                 addTriple(subj, pred, o.as_iri());
+            else if (o.is_blank_node())     addTriple(subj, pred, o.as_blank_node());
+            else if (o.is_literal())        addTriple(subj, pred, o.as_literal());
+            else assert(false && "triple object invalid");
+
+        } else if (s.is_blank_node()) {
+            const auto& subj = s.as_blank_node();
+
+            if (o.is_iri())                 addTriple(subj, pred, o.as_iri());
+            else if (o.is_blank_node())     addTriple(subj, pred, o.as_blank_node());
+            else if (o.is_literal())        addTriple(subj, pred, o.as_literal());
+            else assert(false && "triple object invalid");
+
+        } else {
+            assert(false && "triple subject invalid");
         }
     }
-    int32_t nodeId(const Node& res) const { return nodes.get_safe(res); }
+    void addTriple(const IRI& s, const IRI& p, const IRI& o);
+    void addTriple(const IRI& s, const IRI& p, const BlankNode& o);
+    void addTriple(const IRI& s, const IRI& p, const Literal& o);
+    void addTriple(const BlankNode& s, const IRI& p, const IRI& o);
+    void addTriple(const BlankNode& s, const IRI& p, const BlankNode& o);
+    void addTriple(const BlankNode& s, const IRI& p, const Literal& o);
+    int32_t nodeId(const IRI& res) const { return nodes.get_safe(res); }
     int32_t relationId(const IRI& iri) const { return relations.get_safe(iri); }
     int32_t typeId(const IRI& t) const { return literalTypes.get_safe(t); }
     static const size_t DEFAULT_CAPACITY = 1024;
-    static const size_t DATATYPE_PROPERTY_KEY = 1;
+private:
+    NodeHelper nodeHelper;
 };
 
 }
--- src/gengodb/semantics/RdfGraph.cpp
+++ src/gengodb/semantics/RdfGraph.cpp
@@ -13,11 +13,65 @@ RdfGraph RdfGraph::create(const IRI& name, void* node_ptr, size_t node_l, void* 
     auto g = RdfGraph::create(name);
     void* dst_node_ptr = runtime::GraphStorageHelper::getNodeBufferPtr((uint8_t*) g.storage);
     memcpy(dst_node_ptr, node_ptr, node_l);
+    g.storage->nodeCounter = node_l / sizeof(runtime::PropertyGraph::NodeEntry);
     void* dst_rel_ptr = runtime::GraphStorageHelper::getRelBufferPtr((uint8_t*) g.storage);
     memcpy(dst_rel_ptr, rel_ptr, rel_l);
+    g.storage->relCounter = rel_l / sizeof(runtime::PropertyGraph::RelationshipEntry);
     void* dst_prop_ptr = runtime::GraphStorageHelper::getPropBufferPtr((uint8_t*) g.storage);
     memcpy(dst_prop_ptr, prop_ptr, prop_l);
+    g.storage->propCounter = prop_l / sizeof(runtime::PropertyGraph::PropertyEntry);
     return g;
+}
+inline void NodeHelper::ensureNode() {
+    if (g->storage->nodeCounter < g->nodes.size()) {
+        auto id = g->storage->addNode();
+        g->storage->addNodeProperty(id, -1, -1, id);
+    }
+}
+inline int32_t NodeHelper::resolveNode(const BlankNode& b) {
+    const auto ident = b.identifier();
+    auto it = g->bnodes.find(ident);
+    if (it != g->bnodes.end())
+        return it->second;
+    auto id = g->nodes.insert(IRI{});
+    ensureNode();
+    g->bnodes.emplace(ident, id);
+    return id;
+}
+inline int32_t NodeHelper::resolveNode(const IRI& iri) {
+    auto id = g->nodes.get_or_insert(iri);
+    ensureNode();
+    return id;
+}
+inline int32_t NodeHelper::resolvePredicate(const IRI& p) {
+    return g->relations.get_or_insert(p);
+}
+inline void NodeHelper::addLiteral(int32_t sid, int32_t pid, const Literal& o) {
+    RdfDatatypeInlineHelper inlineHelper;
+    auto l = o.as_literal();
+    auto datatype = g->literalTypes.get_or_insert(l.datatype());
+    uint64_t v = 0;
+    assert(inlineHelper.isInlined(l.datatype()) && "only inlined literals supported");
+    inlineHelper.inlineValue(&v, l.value(), l.datatype());
+    g->storage->addNodeProperty(sid, pid, datatype, v);
+}
+void RdfGraph::addTriple(const IRI& s, const IRI& p, const IRI& o) {
+    storage->addRelationship(nodeHelper.resolveNode(s), nodeHelper.resolveNode(o), nodeHelper.resolvePredicate(p));
+}
+void RdfGraph::addTriple(const IRI& s, const IRI& p, const BlankNode& o) {
+    storage->addRelationship(nodeHelper.resolveNode(s), nodeHelper.resolveNode(o), nodeHelper.resolvePredicate(p));
+}
+void RdfGraph::addTriple(const IRI& s, const IRI& p, const Literal& o) {
+    nodeHelper.addLiteral(nodeHelper.resolveNode(s), nodeHelper.resolvePredicate(p), o);
+}
+void RdfGraph::addTriple(const BlankNode& s, const IRI& p, const IRI& o) {
+    storage->addRelationship(nodeHelper.resolveNode(s), nodeHelper.resolveNode(o), nodeHelper.resolvePredicate(p));
+}
+void RdfGraph::addTriple(const BlankNode& s, const IRI& p, const BlankNode& o) {
+    storage->addRelationship(nodeHelper.resolveNode(s), nodeHelper.resolveNode(o), nodeHelper.resolvePredicate(p));
+}
+void RdfGraph::addTriple(const BlankNode& s, const IRI& p, const Literal& o) {
+    nodeHelper.addLiteral(nodeHelper.resolveNode(s), nodeHelper.resolvePredicate(p), o);
 }
 void RdfGraphRegistry::add(const RdfGraph& g) {
     knownGraphs.insert(std::make_pair(g.name, g));
@@ -31,9 +85,11 @@ RdfGraph RdfGraphRegistry::get(const IRI& name) const {
 }
 void RdfGraphRegistry::loadAll() {
     // TODO implement
+    assert(false && "not implemented");
 }
 void RdfGraphRegistry::load(const IRI& g) {
     // TODO implement
+    assert(false && "not implemented");
 }
 RdfGraph RdfGraphRegistry::loadFromFile(const std::string& file, const IRI& name) {
     // TODO implement
