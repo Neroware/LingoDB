bde2e1999a70e50ae975ee8a4d4ecaed184ca00e
Raoul Zebisch <raoul.zebischq@uni-a.de>
2026-03-25 14:40:28 +0000

    GPM term attribute def

--- include/gengodb/compiler/Dialect/GPM/IR/GPMOpsAttributes.h
+++ include/gengodb/compiler/Dialect/GPM/IR/GPMOpsAttributes.h
@@ -4,6 +4,8 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 
+#include "gengodb/compiler/Dialect/GPM/IR/GPMOpsEnums.h"
+
 #define GET_ATTRDEF_CLASSES
 #include "gengodb/compiler/Dialect/GPM/IR/GPMOpsAttributes.h.inc"
 
